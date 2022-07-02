383ba93af19203d55b8533833ee98454eb83cf6f
VictorGod <99352615+VictorGod@users.noreply.github.com>
2022-07-02 12:52:29 +0300

    Update prizma.h

--- prizma.h
+++ prizma.h
@@ -9,12 +9,14 @@ using namespace std;
 class prizma
 {
 private:
-	double a, b, c, d, h, h2, A , B ,C ,D ,E ,F ,G ,H;
+	double a , b, c, d, h,h2,S,So,Sb;
+	
+	
 public:
 	prizma();
 	prizma(const prizma& prizma);
-	prizma(double a, double b, double c, double d, double h, double h2, double A, double B, double C, double D, double E, double F, double G, double H);
-	
+	prizma( double a, double b, double c, double d, double h, double h2, double S, double So, double Sb);
+
 
 
 	double get_a() const;
@@ -23,43 +25,37 @@ public:
 	double get_d() const;
 	double get_h() const;
 	double get_h2() const;
-	double get_A() const;
-	double get_B() const;
-	double get_C() const;
-	double get_D() const;
-	double get_E() const;
-	double get_F() const;
-	double get_G() const;
-	double get_H() const;
+	double get_S() const;
+	double get_So() const;
+	double get_Sb() const;
 	
+
 	double set_a(double new_a);
 	double set_b(double new_b);
 	double set_c(double new_c);
 	double set_d(double new_d);
 	double set_h(double new_h);
 	double set_h2(double new_h2);
-	double set_A(double  new_A);
-	double set_B(double  new_B);
-	double set_C(double  new_C);
-	double set_D(double  new_D);
-	double set_E(double  new_E);
-	double set_F(double  new_F);
-	double set_G(double  new_G);
-	double set_H(double  new_H);
-
+	double set_S(double  new_S);
+	double set_So(double  new_So);
+	double set_Sb(double  new_Sb);
 	
 
-	
-	double Volume(double a, double b, double c,double h);//triangle
-	
-	double Volume(double a, double b, double h, double h2);//trapezoid
-	
-	
-	double Surface_area(double a, double b, double c,double h);//triangle
-	
-	double Surface_area(double a, double b, double c,double d, double h,double h2);//trapezoid
+
+
+
+	double Volume_triangle(double a, double b, double c, double h);//triangle
+
+	double Volume_trapezoid(double a, double b, double h, double h2);//trapezoid
+
+
+	double Surface_area(double a, double b, double c, double h);//triangle
+
+	double Surface_area(double a, double b, double c, double d, double h, double h2);//trapezoid
 
 };
+
+
 
 
 #endif 
