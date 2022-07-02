0d32014e12e68b9c14fe85f04ab641b4b267c2ad
Kholodulin <90597757+Kholodulin@users.noreply.github.com>
2022-07-02 16:20:35 +0300

    Update Parallelepiped.h

--- Parallelepiped.h
+++ Parallelepiped.h
@@ -8,8 +8,7 @@ class parallelepiped
 {
 private:
 	double a, b, c, h;
-	double DAB, A1AB, A1AD; //corner
-	double V, S_poln;
+	double DAB, A1AB, A1AD; //corner\
 	string vershini[4][4]{
 		{"A1","B1","C1","D1"},
 		{"A","B","C","D"}
@@ -26,8 +25,6 @@ public:
 	double get_DAB() const;
 	double get_A1AB() const;
 	double get_A1AD() const;
-	double get_V() const;
-	double get_S_poln() const;
 
 	double set_a(double new_a);
 	double set_b(double new_b);
@@ -36,8 +33,6 @@ public:
 	double set_DAB(double new_DAB);
 	double set_A1AB(double new_A1AB);
 	double set_A1AD(double new_A1AD);
-	double set_V(double new_V);
-	double set_S_poln(double new_S_poln);
 
 	double Surface_grani(double a, double b, double corner);
 	double Volume(double a, double b, double h);
