bc41528e748fc0bcf484a5d1d338ad0812fb80d2
Kholodulin <90597757+Kholodulin@users.noreply.github.com>
2022-07-05 12:46:01 +0300

    Update Parallelepiped.h

--- Parallelepiped.h
+++ Parallelepiped.h
@@ -8,14 +8,18 @@ class parallelepiped
 {
 private:
 	double a, b, c, h;
-	double DAB, A1AB, A1AD; //corner\
+	double DAB, A1AB, A1AD; //corner
 	string vershini[4][4]{
 		{"A1","B1","C1","D1"},
 		{"A","B","C","D"}
 	};
 public:
+	string A = "A", B = "B", C = "C", D = "D", A1 = "A1", B1 = "B1", C1 = "C1", D1 = "D1";
+	string versh = A + "," + B + "," + C + "," + D + "," + A1 + "," + B1 + "," + C1 + "," + D1;
+	string grani = A + B + C + D + ", " + A1 + B1 + C1 + D1 + ", " + A + A1 + B1 + B + ", " + B + B1 + C1 + C + ", " + C + C1 + D1 + D + ", " + D + D1 + A1 + A;
+
 	parallelepiped();
-	parallelepiped(double _a, double _b, double _c, double _h, double _DAB, double _A1AB, double _A1AD, double _V, double _S_poln);
+	parallelepiped(double a, double b, double c, double h, double DAB, double A1AB, double A1AD);
 	parallelepiped(const parallelepiped& parallelepiped);
 
 	double get_a() const;
@@ -40,5 +44,9 @@ public:
 	
 	double Surface_area(double a, double b, double c);
 	double Surface_area(double a, double b, double c, double DAB, double A1AB, double A1AD);
+
+	string Grani(string grani);
+
+	string Vershin(string versh);
 
 };
